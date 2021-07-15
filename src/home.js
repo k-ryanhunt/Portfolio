289324b88fa21efb443fbfc376c923e259e891d8
K Ryan <hunt.kelsearyan@gmail.com>
2021-07-15 13:54:02 -0600

    Added projects and links to GitHub

--- src/home.js
+++ src/home.js
@@ -6,7 +6,60 @@ class Home extends Component {
     return (
       <div>
         <h2>hello!</h2>
-        
+        <br></br>
+        <p>
+          As someone who has always had a passion for creativity, I have
+          explored many different jobs and opportunities to try and find my
+          niche. I originally started my years after high school in college
+          studying Commercial Graphics, specifically Graphic Design. I was in
+          love with the creative process and being able to create something
+          functional yet beautiful for other people. I briefly took a hiatus
+          from the design world due to personal reasons and I regretted it
+          everyday. During that time, I freelanced logos for companies using
+          just Microsoft PowerPoint and my passion for design. It didn’t stop
+          there either. I was contracted to decorate people’s homes, redesign
+          forms for my job that were more functional and aesthetically pleasing,
+          and I created advertisements that are now used as an example for a
+          school project.
+        </p>
+        <br></br>
+        <p>
+          When I found web design, I knew it was something that I had to
+          explore. Being able to create something that is functional, effective,
+          and eye-catching is exactly what I had been yearning for. Once I
+          started the UX/UI Design program through University of Denver, I
+          finally felt like I found something that encompassed all of my
+          passions into one. I have already gone on about my love for design but
+          what I also love is a good challenge. I enjoy sweeping through code
+          and finding what is making it break, I enjoy the trial and error
+          process of trying to make certain features work, and I love patterns.
+          I love patterns, repetition, and the analytical side of me also loves
+          this field. During group projects, I was either managing what features
+          needed to be done next, what could make the design more appealing to a
+          potential client, or debugging code to figure out what was making it
+          break.
+        </p>
+        <br></br>
+        <p>
+          I am typically promoted quickly within my companies or chosen to be in
+          a management position. I am extremely dedicated to being efficient and
+          always finding new ways to make things functional. I find creative
+          solutions to workplace problems and I pick up on new skills quickly. I
+          was elected President of the Dental Hygiene program that I was in,
+          recognized by two separate Wells Fargo’s Regional Presidents for
+          having high performance, and received recognition awards at
+          Benefitfocus for my quarterly reviews.
+        </p>
+        <br></br>
+        <p>
+          I may not have formal experience in UX/UI Design but I will be
+          successful given the right opportunity with the right company. I have
+          the desire, passion, and eye for design as I am also continuously
+          researching new ideas and code languages. My goal is to work with
+          clients and come up with an aesthetically pleasing and functional
+          design for everyone. The victories of consistently improving and
+          bettering a product or website for the user is thrilling to me.
+        </p>
       </div>
     );
   }
--- src/stuff.js
+++ src/stuff.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import "materialize-css";
 import { Carousel } from "react-materialize";
-import project1 from "./images/placeholder/Slide6.PNG";
+import project1 from "./images/placeholder/Slide6.png";
 import project2 from "./images/placeholder/Slide7.PNG";
 import project3 from "./images/placeholder/Slide8.PNG";
 import project4 from "./images/placeholder/Slide9.PNG";
@@ -20,17 +20,17 @@ class Stuff extends Component {
             indicators: true,
           }}
         >
-          <a className="carousel-item" href="#one!">
-            <img src={project1} alt="Nature" />
+          <a className="carousel-item" href="https://github.com/k-ryanhunt/Project-One-Concert-Finder">
+            <img src={project1} alt="Concert Finder Project" />
           </a>
-          <a className="carousel-item" href="#two!">
-            <img src={project2} alt="Nature" />
+          <a className="carousel-item" href="https://github.com/k-ryanhunt/Project-2-Travel-Green">
+            <img src={project2} alt="Tripr Project" />
           </a>
-          <a className="carousel-item" href="#three!">
-            <img src={project3} alt="Nature" />
+          <a className="carousel-item" href="https://github.com/k-ryanhunt/Timed-Quiz">
+            <img src={project3} alt="Timed Quiz" />
           </a>
-          <a className="carousel-item" href="#four!">
-            <img src={project4} alt="Nature" />
+          <a className="carousel-item" href="https://github.com/k-ryanhunt/Day-Planner">
+            <img src={project4} alt="Day Planner" />
           </a>
           <a className="carousel-item" href="#five!">
             <img src={project5} alt="Nature" />
